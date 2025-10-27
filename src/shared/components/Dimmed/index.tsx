50525bfa94226c0c0a997092775a6c39a7ecf353
Dohyeop Lim <dhlim0817@gmail.com>
2025-10-28 00:13:46 +0900

    feat: add dimmed background

---
+++ src/shared/components/Dimmed/index.tsx
@@ -0,0 +1,50 @@
+import { motion } from "motion/react";
+
+type DimmedProps = {
+    zIndex?: number;
+    on?: boolean;
+    opacity?: number;
+    onClick?: () => void;
+};
+
+export default function Dimmed({
+    zIndex = 998,
+    on = false,
+    opacity = 0.5,
+    onClick,
+}: DimmedProps) {
+    const backdropVariants = {
+        off: {
+            opacity: 0,
+            height: "0vh",
+            transition: {
+                height: { delay: 0.3 },
+                opacity: { duration: 0.3 },
+            },
+            transitionEnd: { display: "none", pointerEvents: "none" },
+        },
+        on: {
+            display: "block",
+            pointerEvents: "auto",
+            opacity,
+            height: "100vh",
+            transition: {
+                height: { duration: 0 },
+                opacity: { duration: 0.3 },
+            },
+        },
+    };
+
+    return (
+        <motion.div
+            className={`fixed w-screen bg-black z-${zIndex}`}
+            variants={backdropVariants}
+            animate={on ? "on" : "off"}
+            initial="off"
+            onClick={(e) => {
+                e.preventDefault();
+                onClick?.();
+            }}
+        />
+    );
+}
--- src/shared/components/Header/index.tsx
+++ src/shared/components/Header/index.tsx
@@ -4,26 +4,31 @@ import { SMOOOTH } from "@/styles/transitions";
 import Logo from "./Logo";
 import Menu from "./Menu";
 import MenuButton from "./MenuButton";
+import Dimmed from "../Dimmed";
 
 export default function Header() {
     const { expanded, toggle, close, contentMotion } = useCollapsible({});
 
     return (
-        <div className="fixed w-full flex justify-center bg-white z-999 shadow-xs">
-            <motion.div
-                layout
-                className="flex w-full max-w-2xl px-4 flex-col"
-                transition={SMOOOTH}
-            >
-                <div className="flex w-full h-16 items-center justify-between">
-                    <Logo />
-                    <MenuButton expanded={expanded} onToggle={toggle} />
-                </div>
+        <>
+            <div className="fixed w-full flex justify-center bg-white z-999 shadow-xs">
+                <motion.div
+                    layout
+                    className="flex w-full max-w-2xl px-4 flex-col"
+                    transition={SMOOOTH}
+                >
+                    <div className="flex w-full h-16 items-center justify-between">
+                        <Logo />
+                        <MenuButton expanded={expanded} onToggle={toggle} />
+                    </div>
 
-                <motion.div {...contentMotion}>
-                    <Menu close={close} />
+                    <motion.div {...contentMotion}>
+                        <Menu close={close} />
+                    </motion.div>
                 </motion.div>
-            </motion.div>
-        </div>
+            </div>
+
+            <Dimmed on={expanded} opacity={0.2} onClick={close} />
+        </>
     );
 }
