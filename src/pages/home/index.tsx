f0c1a172dae854369d6c75e6936d97884beb69fc
Dohyeop Lim <dhlim0817@gmail.com>
2025-11-01 02:22:38 +0900

    feat: implement action sheet and storeinfoform

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,38 +1,60 @@
+import { useState } from "react";
 import SummaryCard from "@/shared/components/SummaryCard";
 import RainbowInlineChart from "@/shared/components/InlineCharts/Rainbow";
 import BannerWithIcon from "@/shared/components/BannerWithIcon";
+import ActionSheet from "@/shared/components/ActionSheet";
+import StoreInfoForm from "./components/StoreInfoForm";
 
 export default function HomePage() {
+    const [showSheet, setShowSheet] = useState(true);
+
+    const onCloseSheet = () => {
+        setShowSheet(false);
+    };
+
     return (
-        <div className="w-full grid grid-cols-1 gap-2">
-            <BannerWithIcon
-                title="핵심 고객 변화"
-                description="30대 고객 방문 비중이 하락하고 있습니다!"
-                label="위험 신호"
-                buttonLabel="AI 추천 솔루션"
-            />
-
-            <SummaryCard
-                value="안전"
-                label="위험 수준 분석"
-                visual={<RainbowInlineChart value={0.2} />}
-            />
-
-            <div className="w-full grid grid-cols-2 gap-2">
-                <SummaryCard
-                    value="1203만원"
-                    delta="1.3%"
-                    trend="down"
-                    label="전월 대비 매출"
-                    animateDelay={0.5}
+        <>
+            <div className="w-full grid grid-cols-1 gap-2">
+                <BannerWithIcon
+                    title="핵심 고객 변화"
+                    description="30대 고객 방문 비중이 하락하고 있습니다!"
+                    label="위험 신호"
+                    buttonLabel="AI 추천 솔루션"
                 />
 
                 <SummaryCard
-                    value="4.7%"
-                    label="상권 고정 비용 상승률"
-                    animateDelay={0.5}
+                    value="안전"
+                    label="위험 수준 분석"
+                    visual={<RainbowInlineChart value={0.2} />}
                 />
+
+                <div className="w-full grid grid-cols-2 gap-2">
+                    <SummaryCard
+                        value="1203만원"
+                        delta="1.3%"
+                        trend="down"
+                        label="전월 대비 매출"
+                        animateDelay={0.5}
+                    />
+
+                    <SummaryCard
+                        value="4.7%"
+                        label="상권 고정 비용 상승률"
+                        animateDelay={0.5}
+                    />
+                </div>
+
+                <button onClick={() => setShowSheet(true)}>Sheet</button>
             </div>
-        </div>
+
+            <ActionSheet
+                title="맛닭꼬끼오 공릉점"
+                isOpen={showSheet}
+                onClose={onCloseSheet}
+                disableDrag
+            >
+                <StoreInfoForm afterSubmit={onCloseSheet} />
+            </ActionSheet>
+        </>
     );
 }
